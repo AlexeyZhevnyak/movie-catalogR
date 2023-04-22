12552e4a770e56cfb38b8a34988361ff18dec818
AlexeyZhevnyak <leksa680@gmail.com>
2023-04-22 16:54:21 +0300

    fixes

--- src/pages/movie/MoviePage.tsx
+++ src/pages/movie/MoviePage.tsx
@@ -1,19 +1,23 @@
 import {useParams} from "react-router";
 import axios from "axios";
 import {useEffect} from "react";
+import styles from "./MoviePage.module.scss"
 
 export const MoviePage = () => {
     const {id} = useParams();
     useEffect(() => {
-        fetch(`http://localhost:3000/movies/${id}`)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    console.log(result);
-                    // setMovies(result)
-                })
-    }, [])
-    return <div>
-        aga
+        axios.get(`http://localhost:3000/movies/${id}`)
+            .then(response => {
+                console.log(response.data);
+                // setMovies(response.data);
+            })
+    }, []);
+    return <div className={styles.wrapper}>
+        <div className={styles.main_content}>
+            <p>Содержимое вашего центрированного div здесь</p>
+            <p>Содержимое вашего центрированного div здесь</p>
+            <p>Содержимое вашего центрированного div здесь</p>
+            <p>Содержимое вашего центрированного div здесь</p>
+        </div>
     </div>;
 }
